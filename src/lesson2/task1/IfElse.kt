ba756fbbb38bc499442ba3f8276d5a0cc4d41c44
youngtfue <notttvova@gmail.com>
2018-10-17 23:29:45 +0300

    Решил 2 лесон

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -2,6 +2,7 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.sqrt
 
@@ -62,7 +63,12 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String = when {
+    (age % 10 == 1) -> "$age год"
+    (age % 10 in 2..4) -> "$age года"
+    (age % 100 in 5..20) -> "$age лет"
+    else -> "$age лет"
+}
 
 /**
  * Простая
@@ -73,7 +79,18 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    val s1 = v1 * t1
+    val s2 = v2 * t2
+    val s3 = v3 * t3
+    val sfull = s1 + s2 + s3
+    val shalf = sfull / 2
+    return when {
+        s1 >= shalf -> shalf / v1
+        s1 + s2 >= shalf -> (shalf - s1) / v2 + t1
+        else -> (shalf - s1 - s2) / v3 + t1 + t2
+    }
+}
 
 /**
  * Простая
@@ -86,7 +103,16 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+    val lodya1 = kingX == rookX1 || kingY == rookY1
+    val lodya2 = kingX == rookX2 || kingY == rookY2
+    return when {
+        lodya1 && lodya2 -> 3
+        lodya1 -> 1
+        lodya2 -> 2
+        else -> 0
+    }
+}
 
 /**
  * Простая
@@ -100,7 +126,16 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
  */
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
-                          bishopX: Int, bishopY: Int): Int = TODO()
+                          bishopX: Int, bishopY: Int): Int {
+    val lodya = kingX == rookX || kingY == rookY
+    val slon = abs(kingX - bishopX) == abs(kingY - bishopY)
+    return when {
+        lodya && slon -> 3
+        slon -> 2
+        lodya -> 1
+        else -> 0
+    }
+}
 
 /**
  * Простая
@@ -110,7 +145,17 @@ fun rookOrBishopThreatens(kingX: Int, kingY: Int,
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    val max = maxOf(a, b, c)
+    val min = minOf(a, b, c)
+    val average = a + b + c - max - min
+    if (max > min + average) return -1
+    return when {
+        max * max == min * min + average * average -> 1
+        max * max < min * min + average * average -> 0
+        else -> 2
+    }
+}
 
 /**
  * Средняя
@@ -120,4 +165,11 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    val firstS = maxOf(c, a)
+    val secondS = minOf(b, d)
+    return when {
+        c > b || a > d -> -1
+        else -> secondS - firstS
+    }
+}
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -2,6 +2,9 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import lesson4.task1.abs
+import kotlin.math.abs
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -17,8 +20,11 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
-
+fun isNumberHappy(number: Int): Boolean {
+    val s1 = (number / 10) % 10 + number % 10
+    val s2 = (number % 1000) / 100 + number / 1000
+    return s1 == s2
+}
 /**
  * Простая
  *
@@ -26,7 +32,8 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+        abs(x1 - x2) == abs(y1 - y2) || x1 == x2 || y1 == y2
 
 
 /**
@@ -35,7 +42,13 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int = when {
+    (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && month == 2 -> 29
+    month == 2 -> 28
+    month > 8 && month % 2 != 0 -> 30
+    month in 3..7 && month % 2 == 0 -> 30
+    else -> 31
+}
 
 /**
  * Средняя
@@ -45,7 +58,7 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean = sqrt(sqr(x1 - x2) + sqr(y1 - y2)) + r1 <= r2
 
 /**
  * Средняя
@@ -56,4 +69,11 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    val maxS = maxOf(a, b, c)
+    val minS = minOf(a, b, c)
+    val averageS = a + b + c - minS - maxS
+    val maxRS = maxOf(r, s)
+    val minRS = minOf(r, s)
+    return maxRS >= averageS && minRS >= minS
+}
